4960bb8d4a8da28a2694f8e21d56db563282f363
Deniss Kaibagarovs <deniss.kaibagarovs@gmail.com>
2013-12-24 23:25:13 +0200

    Some UI fix

--- fuel/app/views/users/login.php
+++ fuel/app/views/users/login.php
@@ -1,19 +1,23 @@
 <h2>Login</h2>
-<p>Login to your account using your username and password.</p>
-
+<p>Login to your account using your username and password. </p>
 <?php echo isset($errors) ? $errors : false; ?>
 <?php echo Form::open('users/login'); ?>
-
 <div class="input text required">
-    <?php echo Form::label('Username', 'username'); ?>
-    <?php echo Form::input('username', isset($username) ? $username : false, array('size' => 30)); ?>
+	<span style="padding:0px 10px 0px 0px;"> 
+    	<?php echo Form::label('Username', 'username'); ?>
+    </span>
+    <?php echo Form::input('username', NULL, array('size' => 30)); ?>
 </div>
-
 <div class="input password required">
-    <?php echo Form::label('Password', 'password'); ?>
+	<span style="padding:0px 12px 0px 0px;"> 
+    	<?php echo Form::label('Password', 'password'); ?>
+    </span>
     <?php echo Form::password('password', NULL, array('size' => 30)); ?>
 </div>
-
 <div class="input submit">
-    <?php echo Form::submit('login', 'Login'); ?>
+	<span style="padding:0px 0px 0px 110px;"> 
+   		<?php echo Form::submit('login', 'Login'); ?>
+   	</span>
 </div>
+
+<?php echo Form::close(); ?>
