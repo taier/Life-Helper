ef221c0da6d96a29a901d535954267bb0e7e665b
Arthurio93 <murz1k@inbox.lv>
2014-01-20 20:57:59 +0200

    simpleauth2

--- fuel/app/views/account/simpleauth.php
+++ fuel/app/views/account/simpleauth.php
@@ -6,18 +6,19 @@
  <style type="text/css">
   body { background-image: url(http://wallpaper.pickywallpapers.com/1680x1050/highlighted-grey-background.jpg); margin: 45px 0 0 0; font-family: ‘Palatino Linotype’, ‘Book Antiqua’, Palatino, serif; font-size: 18px }
   #wrapper { width: 740px; margin: 0 auto; }
-  h1 { color: #333333; font: normal normal normal 62px/1em Impact, Charcoal, sans-serif; margin: 0 0 15px 0; }
+  h1 { color: #ffffff; font: normal normal normal 62px/1em Impact, Charcoal, sans-serif; margin: 0 0 15px 0; }
   pre { padding: 15px; background-color: #FFF; border: 1px solid #CCC; font-size: 16px;}
   #footer p { font-size: 14px; text-align: right; }
   a { color: #ffffff; }
-  #register {color: #ffffff; }
+  label {color: #ffffff;}
  </style>
 </head>
 
 <?php
 echo Form::open();
-echo Form::fieldset_open(null, "User data");
+echo Form::fieldset_open(null, "");
 ?>
+<h1>Login</h1>
 <label for="username">User name</label>
     <input type="text" name="username" id="username" />
 </br>
