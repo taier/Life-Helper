201529e3ec2a79b3063e84f81d3deb1f04c34c85
Arthurio93 <murz1k@inbox.lv>
2014-01-20 22:31:55 +0200

    Всё что смог - поправил

--- fuel/app/views/templates/index.php
+++ fuel/app/views/templates/index.php
@@ -6,10 +6,10 @@
  <style type="text/css">
   body { background-image: url(http://wallpaper.pickywallpapers.com/1680x1050/highlighted-grey-background.jpg); margin: 45px 0 0 0; font-family: ‘Palatino Linotype’, ‘Book Antiqua’, Palatino, serif; font-size: 18px }
   #wrapper { width: 740px; margin: 0 auto; }
-  h1 { color: #333333; font: normal normal normal 62px/1em Impact, Charcoal, sans-serif; margin: 0 0 15px 0; }
+  h1 { color: #ffffff; font: normal normal normal 62px/1em Impact, Charcoal, sans-serif; margin: 0 0 15px 0; }
   pre { padding: 15px; background-color: #FFF; border: 1px solid #CCC; font-size: 16px;}
   #footer p { font-size: 14px; text-align: right; }
-  a { color: #000; }
+  a { color: #ffffff; text-decoration:none;}
  </style>
 </head>
 
